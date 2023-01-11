c74bb901ec19ae8abb2530c8e2913b4381ac49ad
mark <mark@pop-os.localdomain>
2023-01-10 22:26:21 -0500

    New file added

--- python/test/new_config_chg_script.py
+++ python/test/new_config_chg_script.py
@@ -3,11 +3,15 @@ from getpass import getpass
 import time
 from netmiko import ConnectHandler
 
+from netmiko import NetMikoTimeoutException
+from paramiko.ssh_exception import SSHException
+from netmiko import NetmikoAuthenticationException
+
 username = input("Enter username: ")
 password = getpass()
 LIST = input("Enter List name: ")
 
-listname = "/LISTS/%s.txt"  % LIST
+listname = "/LISTs/%s.txt"  % LIST 
 
 f = open (listname)
 
@@ -19,23 +23,40 @@ for line in f:
              'username': username,
              'password': password,
            }
-
-    net_connect = ConnectHandler(**iosv_l2)
+         
+    try:
+      net_connect = ConnectHandler(**iosv_l2)
+    except (NetmikoAuthenticationException):
+        print ('Authentication failure: ' + str(host))
+        continue
+    except (NetMikoTimeoutException):
+        print ('Timeout to device: ' + str(host))
+        continue
+    except (EOFError):
+        print ("End of file while attempting device " + str(host))
+        continue
+    except (SSHException):
+        print ('SSH Issue. Are you sure SSH is enabled ? ' + str(host))
+        continue
+    except Exception as unknown_error:
+        print ('Some other error or end of script: ' + str(unknown_error))
+        continue
+   
     net_connect.send_command_timing('conf t\n')
     net_connect.send_command_timing('ip name-server 192.168.250.210\n')
     net_connect.send_command_timing('no ip name-server 192.165.250.210\n')      
     net_connect.send_command_timing('end\n')
     net_connect.send_command_timing('wr mem\n')
-    time.sleep(3)
-    output0 = net_connect.send_command_timing('show run | inc hostname\n')
     time.sleep(1)
-    output1 = net_connect.send_command_timing('show run | inc ip name-server\n')
+    output1 = net_connect.send_command_timing('show run | inc ip name-server')
     time.sleep(1)
     print(" ")
-    print("The DNS Server configuration has been updated for device " + output0[8:20] + " " + host )
+    output2 = net_connect.send_command_timing('show run | inc hostname')
+    time.sleep(1)
+    print("The DNS Server configuration has been updated for the device  at ip address " + " " + host + output2[8:16])
     print(" ")
     print(output1)
-    print (" ")
-
+    print(" ")
+    
 print ("Update completed for all devices...")
-time.sleep(10)
+time.sleep(1)
