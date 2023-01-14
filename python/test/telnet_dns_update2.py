c1c658b2400a9ebc9f8b83aef7ba957331bb528e
markdawkins <netconexxions@gmail.com>
2023-01-14 01:42:06 +0000

    add new file

--- python/test/telnet_dns_update2.py
+++ python/test/telnet_dns_update2.py
@@ -2,27 +2,35 @@ import telnetlib
 from getpass import getpass
 import time 
 
-HOST = input("router_ip_address: " )
 USER = input("username: ")
 PASSWORD = getpass ("password:  ")
 
-tn = telnetlib.Telnet(HOST)
+LIST = input("Enter List name: ")
 
-tn.read_until(b"Username: ")
-tn.write(USER.encode('ascii') + b"\n")
+listname = "./LISTs/%s.txt"  % LIST 
 
-tn.read_until(b"Password: ")
-tn.write(PASSWORD.encode('ascii') + b"\n")
+f = open (listname)
 
-tn.write(b"conf t\n")
-tn.write(b"ip name-server 8.8.8.8\n ")
-tn.write(b" end \n")
-#output = tn.read_until(b"443")
-#print(output.decode('ascii'))
-time.sleep(2)
-tn.write(b" write mem\n")
-time.sleep(2)
-tn.close()
+for line in f:
+    HOST = line.strip()
 
-print(" DNS configuration update completed ")
-time.sleep(5)
+    tn = telnetlib.Telnet(HOST)
+
+    tn.read_until(b"Username: ")
+    tn.write(USER.encode('ascii') + b"\n")
+
+    tn.read_until(b"Password: ")
+    tn.write(PASSWORD.encode('ascii') + b"\n")
+
+    tn.write(b"conf t\n")
+    tn.write(b"ip name-server 8.8.4.4\n ")
+    tn.write(b" no ip name-server 192.168.250.210 \n")
+    tn.write(b" end \n")
+    time.sleep(2)
+    tn.write(b" write mem\n")
+    time.sleep(2)
+    tn.close()
+
+    print(" DNS configuration update completed ")
+    time.sleep(2)
+print (" Script completed ")
