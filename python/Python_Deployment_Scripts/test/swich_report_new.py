6fedb34e61d4eb9333ea95d15445468175442d84
markdawkins <44737733+markdawkins@users.noreply.github.com>
2023-04-26 21:06:10 -0400

    Add files via upload

---
+++ python/Python_Deployment_Scripts/test/swich_report_new.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+from netmiko import ConnectHandler
+from getpass import getpass
+from time import sleep
+
+#device_name = input("Enter device name: ")
+username = input("Enter username: ")
+ 
+HOST = input("Enter host IP: ")
+
+iosv_l2 = {
+    'device_type': 'cisco_ios',
+    'ip':(HOST),
+    'username': username,
+    'password': getpass('Enter SSH password: '),
+}
+
+net_connect = ConnectHandler(**iosv_l2)
+sleep(1)
+net_connect.send_command('\n')
+
+#net_connect.find_prompt()
+output = net_connect.send_command('show ip interface brief')
+sleep(1)
+output1 = net_connect.send_command('show cdp nei | inc SEP')
+sleep(1)
+output2 = net_connect.send_command('show ver')
+sleep(3)
+output3 = net_connect.send_command('show mac address-table int gig 1/0/47')
+sleep(.5)
+output4 = net_connect.send_command('show interface | include (is  up|rate )')
+sleep(.5)
+output5 = net_connect.send_command('show log | inc FAILED')
+sleep(1)
+output6 = net_connect.send_command('show cdp nei')
+
+print (output) 
+print (" ")
+print (output1)
+print (" ")
+print (output2) 
+print (" ")
+print (output3) 
+print (" ")
+print (output4) 
+print (" ")
+print (output5)   
+print (" ")
+print (output6)
+
+net_connect.send_command('end\n')
